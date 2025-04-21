ccfe7348e26fadbc133d4ce614911d5bbcc524a0
Hanzla Sibghat <hanzlasib@gmail.com>
2025-04-21 12:16:36 +0200

    Creating models

---
+++ companyapi/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+# Create compant models
+class Company(models.Model):
+    company_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    location=models.CharField(max_length=50)
+    about=models.TextField()
+    type=models.CharField(max_length=100,choices=(('IT','IT'),("Non It","Non It"),("Mobiles Phones","Mobiles Phones")))
+    added_date=models.DateTimeField(auto_now=True)
+    active=models.BooleanField(default=True)
